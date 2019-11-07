3f7c269b5d310dca291eba566b7b54250ffa4c05
sanjana <agrawalsanjana1@gmail.com>
2019-11-07 17:55:50 +0530

    Check if numeric value is entered

--- src/App.js
+++ src/App.js
@@ -2,25 +2,43 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      year: 'year',
+      isLeapYear: ''
+    }
+   }
+
+  checkIfItsLeapYear = e => {
+    this.setState({ year: e.target.value });
+    this.checkIfItsANumber(e.target.value);
+  }
+
+  checkIfItsANumber = enteredNumber => {
+    let userInput = enteredNumber;
+    const numberPattern = new RegExp("^[0-9]+$");
+    if (numberPattern.test(userInput)) {
+      this.setState({ isLeapYear: '' });
+    }
+    else {
+      this.setState({ isLeapYear: 'Enter a number' });
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h3>Check if Entered Year is a Leap Year</h3>
+        <h4>Enter Year:</h4>
+        <input type="text" maxLength="6" defaultValue={this.state.year}
+          onChange={this.checkIfItsLeapYear}></input>
+        <h4>{this.state.isLeapYear}</h4>
+      </div >
+    );
+  }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Adapter from 'enzyme-adapter-react-16';
+import { configure, shallow, mount } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+configure({ adapter: new Adapter() });
+
+const appWrapper = shallow(<App />);
+const appInstance = appWrapper.instance();
+
+describe('App Component', () => {
+  it('renders without crashing', () => {
+    shallow(<App />);
+  });
+});
+
+describe('Check if it is a leap year', ()=>{
+ 
+  it('should show message if entered value is non-numeric',() => {
+   
+    let enteredYear = "abcd";
+    appInstance.checkIfItsANumber(enteredYear);
+    expect(appInstance.state.isLeapYear).toEqual("Enter a number");   
+  });
 });
